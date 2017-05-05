ed3a8c123a1c97f2d7be349eb5bbf91181fb6ab0
chrisbuttacavoli <Chris.Buttacavoli@a-dec.com>
2017-05-05 16:01:43 -0700

    added oauth handler

--- myapp.py
+++ myapp.py
@@ -1,9 +1,15 @@
 import os
 import webapp2
 import jinja2
+import json
+import logging
  
 jinja_environment = jinja2.Environment(autoescape=True, loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
- 
+googleServer = "https://accounts.google.com/o/oauth2/v2/auth"
+
+#client ID: 1075487943853-i6t2ua56n8oo8960cmkhu7tona5u9bh9.apps.googleusercontent.com
+#client secret: JcIf1764iYwahLxT9P5GBBYa
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
 		template_values = {
@@ -13,6 +19,12 @@ class MainPage(webapp2.RequestHandler):
 		template = jinja_environment.get_template('index.html')
 		self.response.out.write(template.render(template_values))
 
+
+class OAuthHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.write(googleServer)
 		
 app = webapp2.WSGIApplication([
-    ('/', MainPage), ], debug=True)
+    ('/', MainPage),
+	('/oauth', OAuthHandler),
+], debug=True)
