b025be8de23acbaac7a8c858b2b5e3759fa18ff6
chrisbuttacavoli <Chris.Buttacavoli@a-dec.com>
2017-05-05 22:24:17 -0700

    Final?

--- myapp.py
+++ myapp.py
@@ -8,67 +8,61 @@ import logging
 import string
 import random
 import httplib
+import urllib
  
 jinja_environment = jinja2.Environment(autoescape=True, loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 GOOGLE_SERVER = "https://accounts.google.com/o/oauth2/v2/auth"
 
 CLIENT_ID="1075487943853-i6t2ua56n8oo8960cmkhu7tona5u9bh9.apps.googleusercontent.com"
 CLIENT_SECRET = "JcIf1764iYwahLxT9P5GBBYa"
-REDIRECT_URI = "https://oauth2-166722.appspot.com/oauth"
+REDIRECT_URI = "https://oauth2-166722.appspot.com"
+#REDIRECT_URI = "http://localhost:8080"
 
 TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token"
+GPLUS_URL = "https://www.googleapis.com/plus/v1/people/me"
 
-
-# https://accounts.google.com/o/oauth2/v2/auth?
-# client_id=1075487943853-i6t2ua56n8oo8960cmkhu7tona5u9bh9.apps.googleusercontent.com&
-# redirect_uri=https://oauth2-166722.appspot.com/oauth&
-# response_type=code&
-# scope=email&
-# state=SuperSecret9000
-
-#output code: 4/O6WM1q_gA423I_fqIC1VSOJWRCHGuThKsivV8bDbTVg#
-
-#token: ya29.GltBBOqKeBm6LY1EftxYGXpGYNHs8FhoI_1yZhDms7mGQ-rwMxBGaxKNjhTYwVCdo1xqNNh-NJf6AdOJO94RSBQz-lbbs5yQpfyWUO4ct1SFPRd8rS4M4kAAflfL
 
 def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 
 def request_token(self, code):
-	headers = {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
+	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 	payload = { \
-		'code':code, \
+		'code': code, \
 		'client_id': CLIENT_ID, \
 		'client_secret': CLIENT_SECRET, \
 		'redirect_uri': REDIRECT_URI, \
 		'grant_type': 'authorization_code'
 	}
-	result = urlfetch.fetch(\
-		url=TOKEN_URL, \
-		payload=payload, \
-		method=urlfetch.POST, \
-		headers=headers)
+	encoded_data = urllib.urlencode(payload)
+	result = urlfetch.fetch(url=TOKEN_URL, payload=encoded_data, method=urlfetch.POST, headers=headers)
+	
 	return result
+
+
+def get_plus_info(body_json):
+	token = body_json['access_token']
+	headers = {'Authorization': 'Bearer ' + token}
+	result = urlfetch.fetch(url=GPLUS_URL, method=urlfetch.GET, headers=headers)
+	plus_json = json.loads(result.content)
+	return plus_json
 	
+def handle_oauth(self, code, state):
+	token_context = request_token(self, code)
+	body_json = json.loads(token_context.content)
+	plus_json = get_plus_info(body_json)
 	
-def handle_oauth(self, code):
-	http = request_token(self, code)
+	logging.info(json.dumps(plus_json))
+	template_values = {
+		'firstName': plus_json['name']['givenName'], \
+		'lastName': plus_json['name']['familyName'], \
+		'googleUrl': plus_json['url'], \
+		'stateVar': state
+	}
 	
-	if (http.status_code > 200):
-		template_values = {
-			'message': 'An error occurred in your request',
-			'info': 'No information returned'
-		}
-	
-	else:
-		template_values = {
-			'message': 'Congratulations! You have been authorized.',
-			'info': 'you are such a boss'
-		}
-		
 	template = jinja_environment.get_template('oauth.html')
+	# self.response.out.write(template.render(template_values))
 	self.response.out.write(template.render(template_values))
 	
 
@@ -90,8 +84,10 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
 		
 		code = self.request.get('code')
-		if (code):
-			handle_oauth(self, code)
+		state = self.request.get('state')
+		if (code and state):
+			#display_main(self)
+			handle_oauth(self, code, state)
 		
 		else:
 			display_main(self)
